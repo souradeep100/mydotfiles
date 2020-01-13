c6fe8f97b1550be0b18e32f53e5198fff9e4634d
souradeep <souradch@gmail.com>
2020-01-14 01:26:23 +0530

    commit

--- setup.sh
+++ setup.sh
@@ -1,26 +1,27 @@
+#use source , to setup with this file :souradch@gmail.com
+MYHOME=$HOME
 sudo apt update
 sudo apt upgrade
 sudo apt install build-essential python python3 ipython ipython3 python-pip \
  python3-pip vim cscope git tmux zsh curl
 sudo apt install python3-venv
-cd $HOME
+cd $MYHOME
 python3 -m venv myvenv
 if [ -e .gitconfig ]
 then
-    cp .gitconfig $HOME/
+    cp -f .gitconfig $MYHOME/
 fi
 if [ -e .vimrc ]
 then
-    cp .vimrc $HOME/
+    cp -f .vimrc $MYHOME/
 fi
 if [ -e .tmux.config ]
 then
-    cp .tmux.config $HOME/
+    cp -f .tmux.config $MYHOME/
 fi
 sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sudo chsh -s /usr/bin/zsh
 if [ -e .zshrc ]
 then
-    cp .zshrc $HOME/
+    cp -f .zshrc $MYHOME/
 fi
-source $HOME/myvenv/bin/activate
