3ea480c1547cfa85c5326c5376c48b77ab4ea25a
souradeep <souradch@gmail.com>
2020-01-14 00:42:31 +0530

    commit

--- docker_install.sh
+++ docker_install.sh
@@ -1,3 +1,4 @@
+#bash
 wget "https://download.docker.com/linux/ubuntu/dists/disco/pool/stable/amd64/containerd.io_1.2.6-3_amd64.deb";
 wget "https://download.docker.com/linux/ubuntu/dists/disco/pool/stable/amd64/docker-ce-cli_19.03.3~3-0~ubuntu-disco_amd64.deb";
 wget "https://download.docker.com/linux/ubuntu/dists/disco/pool/stable/amd64/docker-ce_19.03.3~3-0~ubuntu-disco_amd64.deb";
