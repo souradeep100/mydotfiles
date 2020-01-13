dd70d8ee8595438078b1cbdaef22e7df50cdc59d
souradeep <souradch@gmail.com>
2020-01-14 01:16:41 +0530

    commit

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 sudo apt update
 sudo apt upgrade
-sudo apt install build-essentials python python3 ipython ipython3 python-pip \
- python3-pip vim cscope git tmux
+sudo apt install build-essential python python3 ipython ipython3 python-pip \
+ python3-pip vim cscope git tmux 
+sudo apt install python3-venv
+cd $HOME
+python3 -m venv myvenv
+source $HOME/myvenv/bin/activate
