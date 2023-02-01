e995a987d219a2f548922b25781d5f9c57515969
schakrabarti <schakrabarti@microsoft.com>
2023-02-01 10:39:39 +0530

    fixed nv_setup

--- nv_setup.sh
+++ nv_setup.sh
@@ -1,25 +1,29 @@
+#!/bin/bash
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
-./nvim.appimage
-if [ $? -ne 0 ]
-then
-	./nvim.appimage --appimage-extract
-	./squashfs-root/AppRun --version
-	sudo mv squashfs-root /
-	sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
-fi
+./nvim.appimage --appimage-extract
+./squashfs-root/AppRun --version
+sudo mv squashfs-root /
+sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
 
 # Optional: exposing nvim globally.
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 sudo update-alternatives --install $(which vim) vim $(which nvim) 10
 sudo update-alternatives --config vim
-mkdir -p ~/.config/nvim
-if [[ -f ./init.vim ]]
+set -x
+if [[ -d $HOME/.config/nvim/ ]]
 then
-	cp init.vim  ~/.config/nvim/
+	echo "cont"
+else
+	mkdir -p ~/.config/nvim
+fi
+if [[ -f $HOME/.config/nvim/init.vim ]]
+then
+	cp init.vim  $HOME/.config/nvim/
 else 
-cat > ~/.config/nvim/init.vim <<EOL
+cat > $HOME/.config/nvim/init.vim <<EOL
 set number
 syntax on
 set tabstop=4
