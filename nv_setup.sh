3de0be6dcbdfc08d13cce0e1293549067628c98e
solar_coder <souradeep.bond.007@gmail.com>
2023-08-01 16:36:07 +0530

    Update nv_setup.sh

--- nv_setup.sh
+++ nv_setup.sh
@@ -10,8 +10,8 @@ sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-sudo update-alternatives --install $(which vim) vim $(which nvim) 10
-sudo update-alternatives --config vim
+#sudo update-alternatives --install $(which vim) vim $(which nvim) 10
+#sudo update-alternatives --config vim
 set -x
 if [[ -d $HOME/.config/nvim/ ]]
 then
