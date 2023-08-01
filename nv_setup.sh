917bcfc04d50c24ae4e8363664b476384834c8a8
solar_coder <souradeep.bond.007@gmail.com>
2023-08-01 16:37:13 +0530

    Update nv_setup.sh

--- nv_setup.sh
+++ nv_setup.sh
@@ -64,7 +64,3 @@ sudo apt install -y nodejs
 #CocInstall coc-clangd
 #CocCommand clangd.install
 
-nmap <silent> gd <Plug>(coc-definition)
-nmap <silent> gy <Plug>(coc-type-definition)
-nmap <silent> gi <Plug>(coc-implementation)
-nmap <silent> gr <Plug>(coc-references)
