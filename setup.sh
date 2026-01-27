5cc116ccc7b208394961e1ac92c88d14da62e223
solar_coder <souradeep.bond.007@gmail.com>
2026-01-27 10:16:37 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,23 +17,11 @@ sudo apt-get install git fakeroot build-essential ncurses-dev xz-utils libssl-de
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 cd $MYHOME
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzfa;
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf;
 ~/.fzf/install
 
 curl -sS https://starship.rs/install.sh | ssh
 
-curl -LO https://github.com/wez/wezterm/releases/download/20240203-110809-5046fc22/WezTerm-20240203-110809-5046fc22-Ubuntu20.04.AppImage
-chmod +x WezTerm-20240203-110809-5046fc22-Ubuntu20.04.AppImage
-mkdir ~/bin
-mv ./WezTerm-20240203-110809-5046fc22-Ubuntu20.04.AppImage ~/bin/wezterm
-~/bin/wezterm
-
-
-python3 -m venv myvenv
-if [ ! -e ".gitconfig" ]
-then
-    cp -f $BASE/.gitconfig $MYHOME/
-fi
 if [ ! -e ".vimrc" ]
 then
     cp -f $BASE/.vimrc $MYHOME/
@@ -43,15 +31,7 @@ then
     cp -f $BASE/.tmux.conf $MYHOME/
 fi
 
-sh -c "$(curl -esSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-chsh -s /usr/bin/zsh
-if [ ! -e ".zshrc" ]
-then
-    cp -f $BASE/.zshrc $MYHOME/
-fi
 
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
 
 if [ ! -e "$MYHOME/.ssh/id_rsa.pub" ]
 then
